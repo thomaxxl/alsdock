04a5582c3abdbadb738ad8810f565bde04994f2d
Thomas <thomas.pollet@gmail.com>
2022-01-27 16:28:10 +0100

    workers

--- multiapp/run.sh
+++ multiapp/run.sh
@@ -1,1 +1,1 @@
-gunicorn -w 1 "multiapp:main()" -b 0.0.0.0:5656  --threads 1 --error-logfile - --access-logfile - --reload
+gunicorn -w 6 "multiapp:main()" -b 0.0.0.0:5656  --threads 1 --error-logfile - --access-logfile - --reload
